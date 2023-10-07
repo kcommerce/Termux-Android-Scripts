b2baad23c3690771535f23f1dbb5a620d79b8f23
Sakchart Ngamluan <kcommerce@gmail.com>
2023-10-07 23:30:54 +0700

    reorder pkg install

--- bin/install-base.sh
+++ bin/install-base.sh
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+mkdir "$HOME/.termux/boot"
 BOOT_FILE="$HOME/.termux/boot/start-services"
 
 echo "---------------------------------"
@@ -15,7 +16,22 @@ pkg install termux-services termux-tools termux-api -y
 echo "*** Install Web(8080), SFTP(8021), SSH(8022) "
 pkg install apache2 openssh-sftp-server -y
 pkg install openssh neofetch fish nmap cronie -y
+echo "** Install addtionals: "
+pkg install vim screen mpg123 sox espeak cmus  -y
+pkg install git -y
+echo "-- Start service --"
+source /data/data/com.termux/files/usr/etc/profile.d/start-services.sh 
+echo "--> Install boot services"
+echo "#!/data/data/com.termux/files/usr/bin/bash" > $BOOT_FILE
+echo "termux-wake-lock" >> $BOOT_FILE
+echo "source /data/data/com.termux/files/usr/etc/profile.d/start-services.sh " >> $BOOT_FILE
+chmod +x $BOOT_FILE
+echo "--> Check boot file:"
+ls -al $BOOT_FILE
+cat $BOOT_FILE
 
+echo "Test espeak :"
+espeak "Hello, World"
 echo "--> Enable services"
 sv-enable crond
 sv up crond
@@ -27,17 +43,5 @@ sv-enable sshd
 sv up sshd
 echo "--> Check services using nmap:"
 nmap localhost
-echo "--> Install boot services"
-echo "#!/data/data/com.termux/files/usr/bin/bash" > $BOOT_FILE
-echo "termux-wake-lock" >> $BOOT_FILE
-echo "source /data/data/com.termux/files/usr/etc/profile.d/start-services.sh " >> $BOOT_FILE
-chmod +x $BOOT_FILE
-echo "--> Check boot file:"
-ls -al $BOOT_FILE
-cat $BOOT_FILE
-
-echo "** Install addtionals: "
-pkg install vim screen mpg123 sox espeak cmus  -y
-pkg install git -y
-echo "Test espeak :"
-espeak "Hello, World"
+echo "--> Check Process tree"
+pstree
